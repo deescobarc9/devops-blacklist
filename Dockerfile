db9d58c9f8b49070d3f6531c64e2cd84d45f5b33
David Escobar <de.escobarc1@uniandes.edu.co>
2025-11-02 15:33:55 -0500

    Configure codepipeline

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+# Imagen base
+FROM python:3.11-slim
+
+# Variables de entorno
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+# Crear directorio de la app
+WORKDIR /app
+
+# Instalar dependencias del sistema
+RUN apt-get update && apt-get install -y \
+    build-essential libpq-dev && \
+    rm -rf /var/lib/apt/lists/*
+
+# Copiar dependencias
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copiar el resto del código
+COPY . .
+
+# Exponer el puerto Flask
+EXPOSE 5000
+
+# Comando de ejecución
+CMD ["gunicorn", "--bind", "0.0.0.0:5000", "main:app"]
--- config.py
+++ config.py
@@ -7,11 +7,12 @@ class Config:
     RDS_DB_NAME = os.environ.get('RDS_DB_NAME')
     RDS_USERNAME = os.environ.get('RDS_USERNAME')
     RDS_PASSWORD = os.environ.get('RDS_PASSWORD')
+    DATABASE_URL = os.environ.get('DATABASE_URL', "postgresql://postgres:mysecretpassword@blacklistdb.cgbywy6689hb.us-east-1.rds.amazonaws.com")
 
     if RDS_HOSTNAME:
         SQLALCHEMY_DATABASE_URI = f"postgresql://{RDS_USERNAME}:{RDS_PASSWORD}@{RDS_HOSTNAME}:{RDS_PORT}/{RDS_DB_NAME}"
     else:
-        SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:mysecretpassword@localhost:5432/blacklist_db'
+        SQLALCHEMY_DATABASE_URI = DATABASE_URL
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'super-secret'
